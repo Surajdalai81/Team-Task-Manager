34b9a57354a2df32b6be6b0b738e8cd274c1e42c
Soumyajit Sinha <sinhasoumya2018@gmail.com>
2026-05-08 10:18:48 +0530

    fixed root route

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,10 @@ import projectRoutes from "./routes/projectRoutes.js";
 
 app.use("/api/projects", projectRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Team Task Manager Backend Running");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
