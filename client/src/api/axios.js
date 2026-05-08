b69199a73896a9aea6cd5e12ea5cee53b71ea593
Soumyajit Sinha <sinhasoumya2018@gmail.com>
2026-05-08 13:02:18 +0530

    connect frontend to backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://team-task-manager-production-8859.up.railway.app",
 });
 
 // TOKEN AUTO ADD
