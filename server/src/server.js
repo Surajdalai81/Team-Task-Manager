cc5dfbf3a738d9acb133f152d4467a9060fba02b
Soumyajit Sinha <sinhasoumya2018@gmail.com>
2026-05-08 11:32:53 +0530

    minimal server test

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   return res.status(200).send("Backend Running Successfully");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
